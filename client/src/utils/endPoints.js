9dafe3110d66853323d753050e71a0483b8f5d58
patelsweta2 <sweta0442@gmail.com>
2024-12-11 12:56:04 +0530

    add endPoints

---
+++ client/src/utils/endPoints.js
@@ -0,0 +1,20 @@
+let baseUrl = " http://localhost:8000/api";
+
+export { baseUrl };
+
+const ENDPOINTS = {
+  REGISTER: `${baseUrl}/users/register`,
+  LOGIN: `${baseUrl}/users/login`,
+  ADD_INCOME: `${baseUrl}/incomes`,
+  GET_ALL_INCOME: `${baseUrl}/incomes`,
+  GET_ONE_INCOME: `${baseUrl}/incomes`,
+  UPDATE_INCOME: `${baseUrl}/incomes`,
+  DELETE_INCOME: `${baseUrl}/incomes`,
+  ADD_EXPENSES: `${baseUrl}/expenses`,
+  GET_ALL_EXPENSES: `${baseUrl}/expenses`,
+  GET_ONE_EXPENSE: `${baseUrl}/expenses`,
+  UPDATE_EXPENSE: `${baseUrl}/expenses`,
+  DELETE_EXPENSE: `${baseUrl}/expenses`,
+};
+
+export default ENDPOINTS;
